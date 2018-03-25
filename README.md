42f8e968d235c7d5abfbe0d3a41520b47cf5df6f
Guilherme Martins <adm.gms@gmail.com>
2018-03-25 19:00:55 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+node-crawler
+================
+
+Simple web scraper with node to extract movie information from IMDB:
+
+``` bash
+$ npm install
+$ node server.js
+```
+
+Navigate to http://localhost:8081/scrape and see what happens.
---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var fs      = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+  // url a ser extraida
+  url = 'http://www.imdb.com/title/tt5795086/';
+
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+
+      var title, release, rating;
+      var json = { title : "", release : "", rating : "", image : ""};
+
+      $('.title_wrapper').filter(function(){
+        var data = $(this);
+        title = data.children().first().text().trim();
+        release = data.children().last().children().last().text().trim();
+        image = $('.poster').children().text();
+        
+        json.title = title;
+        json.release = release;
+        json.image = image;
+      })
+
+      $('.ratingValue').filter(function(){
+        var data = $(this);
+        rating = data.text().trim();
+
+        json.rating = rating;
+      })
+    }
+
+    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+      console.log('File successfully written! - Check your project directory for the output.json file');
+    })
+
+    res.send('Check your console!')
+  })
+})
+
+app.listen('8081')
+console.log('look at http://localhost:8081/scrape :D');
+exports = module.exports = app;
